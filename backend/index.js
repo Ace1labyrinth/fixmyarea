b0977506850a51b61221276baca1d7138f97a2ef
Chinedu Nwogbo <chinedunw2@gmail.com>
2025-07-21 13:21:59 +0100

    Fix: changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://timely-babka-c7d84e.netlify.app', // your frontend Netlify URL
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json({ limit:'10mb' }));
 app.use(express.urlencoded({ extended:true, limit:'10mb' }));
 
